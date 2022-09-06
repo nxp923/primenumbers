185cb1e592b23f5e0cf2612ce92df7cf9765216a
spandanapalepu <95872684+Spandanapalepu@users.noreply.github.com>
2022-09-06 15:29:12 -0400

    added new logic

--- checkPrime.py
+++ checkPrime.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 import pdb
 def check_prime(x):
-  if x == 2:
-    print("%s is a prime number" %(x))
-  if x == 0 or x == 1:
-    print("%s is a not prime number" %(x))
-  else:
+  if x >= 2:
     fail = False
     for i in range(2, x):
       if x % i == 0:
@@ -15,5 +11,7 @@ def check_prime(x):
       print("%s is a not prime number" %(x))
     else:
       print("%s is a prime number" %(x))
-
+  else:
+    print("%s is not a prime number" %(x))
+    
 check_prime(int(input("Enter a number: ")))
